ff5db56c7c0eeb9196ffd873f1ca5b9c01e5a5fa
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-02-12 15:22:19 +0600

    Python 12 feb file added

---
+++ w3resource/BasicPart01/problem09.py
@@ -0,0 +1,6 @@
+"""Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+exam_st_date = (11, 12, 2014)
+Sample Output : The examination will start from : 11 / 12 / 2014"""
+
+exam_st_date = (11,12,24)
+print("The examination will start from: %i / %i / %i"%exam_st_date)
