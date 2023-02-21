ae328fa613f06c36bcac84151fe810932e7ef41c
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-02-21 19:47:27 +0600

    Added 21st feb Python file

---
+++ w3resource/BasicPart01/problem21.py
@@ -0,0 +1,9 @@
+""" Write a Python program that determines whether a given number (accepted from the user) is even or odd, 
+and prints an appropriate message to the user."""
+
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
---
+++ w3resource/BasicPart01/problem22.py
@@ -0,0 +1,10 @@
+""" Write a Python program to count the number 4 in a given list."""
+
+def list_count_4(nums):
+    count = 0
+    for num in nums:
+        if num == 4:
+            count = count + 1
+    return count
+
+print(list_count_4([1, 4, 6, 7, 8, 3, 4]))
