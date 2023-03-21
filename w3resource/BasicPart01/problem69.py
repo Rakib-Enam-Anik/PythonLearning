78290ea46ae8c7c3e0f1f1a9079c1f06ca1bba55
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-03-21 23:34:01 +0600

    21st March python file added

---
+++ w3resource/BasicPart01/problem69.py
@@ -0,0 +1,9 @@
+"""Write a Python program to sort three integers without using conditional statements and loops."""
+x = int(input("Inpuit first: "))
+y = int(input("Input second number: "))
+z = int(input("Input third number: "))
+
+a1 = min(x,y,z)
+a3 = max(x, y, z)
+a2 = (x + y+ z) - a1 -a3
+print("Numbers in sorted order: ", a1, a2, a3)
---
+++ w3resource/BasicPart01/problem70.py
@@ -0,0 +1,8 @@
+"""Write a Python program to sort files by date."""
+
+import glob
+import os
+
+files = glob.glob("*.txt")
+files.sort(key=os.path.getmtime)
+print("\n".join(files))
